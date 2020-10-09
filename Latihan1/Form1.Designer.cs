5d600e5fa8240070f47543083a35abf35e8d4886
Rohmad Fajarudin <kzofajar@gmail.com>
2020-10-09 08:20:01 +0700

    Commit Project

---
+++ Latihan1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Latihan1
+{
+    partial class FormTampilNama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnTampil = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(257, 80);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.UseWaitCursor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(176, 80);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 1;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.UseWaitCursor = true;
+            this.btnHapus.Click += new System.EventHandler(this.BtnHapus_Click);
+            // 
+            // btnTampil
+            // 
+            this.btnTampil.Location = new System.Drawing.Point(95, 80);
+            this.btnTampil.Name = "btnTampil";
+            this.btnTampil.Size = new System.Drawing.Size(75, 23);
+            this.btnTampil.TabIndex = 2;
+            this.btnTampil.Text = "Tampil";
+            this.btnTampil.UseVisualStyleBackColor = true;
+            this.btnTampil.UseWaitCursor = true;
+            this.btnTampil.Click += new System.EventHandler(this.BtnTampil_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nama";
+            this.label1.UseWaitCursor = true;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(81, 27);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(251, 20);
+            this.txtNama.TabIndex = 4;
+            this.txtNama.UseWaitCursor = true;
+            // 
+            // FormTampilNama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 130);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTampil);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnClose);
+            this.Name = "FormTampilNama";
+            this.Text = "Program Menampilkan Nama";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnTampil;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNama;
+    }
+}
+
---
+++ Latihan1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan1
+{
+    public partial class FormTampilNama : Form
+    {
+        public FormTampilNama()
+        {
+            InitializeComponent();
+
+            btnHapus.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void BtnTampil_Click(object sender, EventArgs e)
+        {
+            string input = txtNama.Text;
+
+            MessageBox.Show(input, "Hai!");
+
+            btnHapus.Enabled = true;
+        }
+
+        private void BtnHapus_Click(object sender, EventArgs e)
+        {
+            txtNama.Text = "";
+        }
+    }
+}
